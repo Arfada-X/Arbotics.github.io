3d6217ef8dc60cde40175b0c78036fb53d681e0b
Arfada-X <167385948+Arfada-X@users.noreply.github.com>
2025-12-11 08:22:13 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Fade-in saat scroll
-document.addEventListener("DOMContentLoaded", function() {
-  const sections = document.querySelectorAll('.section, .hero');
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => { if(entry.isIntersecting) entry.target.classList.add('show'); });
-  }, { threshold: 0.2 });
-  sections.forEach(sec => observer.observe(sec));
-});
